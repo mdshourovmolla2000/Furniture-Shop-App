1fcb0a2c72e2d908bbb56d91aea70ac3adfe8435
mdshourovmolla2000 <68102562+mdshourovmolla2000@users.noreply.github.com>
2023-05-03 11:13:54 +0600

    splash screen design complete

--- app/src/main/java/com/shourov/furnitureshop/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/shourov/furnitureshop/view/welcome/WelcomeActivity.kt
@@ -1,12 +1,12 @@
-package com.shourov.furnitureshop.view
+package com.shourov.furnitureshop.view.welcome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shourov.furnitureshop.R
 
-class SplashActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_welcome)
     }
 }
