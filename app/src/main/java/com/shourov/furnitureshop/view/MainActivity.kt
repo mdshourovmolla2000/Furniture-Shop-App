e60afc4cdf0be0f8aa11fcc35ac5510894989df8
mdshourovmolla2000 <68102562+mdshourovmolla2000@users.noreply.github.com>
2023-05-03 10:50:02 +0600

    icon added

---
+++ app/src/main/java/com/shourov/furnitureshop/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shourov.furnitureshop.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shourov.furnitureshop.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/shourov/furnitureshop/view/SplashActivity.kt
+++ app/src/main/java/com/shourov/furnitureshop/view/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.shourov.furnitureshop
+package com.shourov.furnitureshop.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shourov.furnitureshop.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
