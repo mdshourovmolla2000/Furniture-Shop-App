ca887bfefd5a2309b1a615ef2bbaa68d78249795
mdshourovmolla2000 <68102562+mdshourovmolla2000@users.noreply.github.com>
2023-05-08 13:19:28 +0600

    exit dialog added

--- app/src/main/java/com/shourov/furnitureshop/view/FavouriteFragment.kt
+++ app/src/main/java/com/shourov/furnitureshop/view/FavouriteFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.shourov.furnitureshop.R
 import com.shourov.furnitureshop.databinding.FragmentFavouriteBinding
 
@@ -18,6 +19,8 @@ import com.shourov.furnitureshop.databinding.FragmentFavouriteBinding
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentFavouriteBinding.inflate(inflater, container, false)
+
+         binding.bottomNavigationHomeMenu.setOnClickListener { findNavController().navigate(R.id.action_favouriteFragment_to_homeFragment) }
 
          return binding.root
     }
--- app/src/main/java/com/shourov/furnitureshop/view/HomeFragment.kt
+++ app/src/main/java/com/shourov/furnitureshop/view/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.shourov.furnitureshop.view
 
+import android.app.AlertDialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.shourov.furnitureshop.R
 import com.shourov.furnitureshop.adapter.SpecialOffersListAdapter
+import com.shourov.furnitureshop.databinding.DialogExitBinding
 import com.shourov.furnitureshop.databinding.FragmentHomeBinding
 import com.shourov.furnitureshop.model.SpecialOfferData
 import com.shourov.furnitureshop.repository.HomeRepository
@@ -20,6 +26,34 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
 
     private val specialOfferItemsList: ArrayList<SpecialOfferData> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val builder = AlertDialog.Builder(requireContext())
+                val dialogBinding = DialogExitBinding.inflate(layoutInflater)
+
+                builder.setView(dialogBinding.root)
+                builder.setCancelable(true)
+
+                val alertDialog = builder.create()
+
+                //make transparent to default dialog
+                alertDialog.window?.setBackgroundDrawable(ColorDrawable(0))
+
+                dialogBinding.noButton.setOnClickListener { alertDialog.dismiss() }
+
+                dialogBinding.yesButton.setOnClickListener {
+                    alertDialog.dismiss()
+                    requireActivity().finish()
+                }
+
+                alertDialog.show()
+            }
+        })
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,6 +70,8 @@ class HomeFragment : Fragment() {
         binding.specialOfferRecyclerview.apply {
             adapter = SpecialOffersListAdapter(specialOfferItemsList)
         }
+
+        binding.bottomNavigationFavouriteMenu.setOnClickListener { findNavController().navigate(R.id.action_homeFragment_to_favouriteFragment) }
 
         return binding.root
     }
