05a7dfcb31ee7f2da5f6bd017cdbbb1097767197
mdshourovmolla2000 <68102562+mdshourovmolla2000@users.noreply.github.com>
2023-05-14 17:58:33 +0600

    Update ProductDetailsViewModel.kt

--- app/src/main/java/com/shourov/furnitureshop/view_model/ProductDetailsViewModel.kt
+++ app/src/main/java/com/shourov/furnitureshop/view_model/ProductDetailsViewModel.kt
@@ -7,21 +7,25 @@ import com.shourov.furnitureshop.model.ProductImageModel
 import com.shourov.furnitureshop.model.ProductModel
 import com.shourov.furnitureshop.repository.ProductDetailsRepository
 
-class ProductDetailsViewModel(private val repository: ProductDetailsRepository): ViewModel() {
+class ProductDetailsViewModel(private val repository: ProductDetailsRepository) : ViewModel() {
     private val _productImagesLiveData = MutableLiveData<List<ProductImageModel?>>()
     val productImageLiveData: LiveData<List<ProductImageModel?>>
         get() = _productImagesLiveData
 
-    fun getProductImages(productId: String?) = _productImagesLiveData.postValue(repository.getProductImages(productId))
+    fun getProductImages(productId: String?) =
+        _productImagesLiveData.postValue(repository.getProductImages(productId))
 
     private val _productDetailsLiveData = MutableLiveData<ProductModel?>()
     val productDetailsLiveData: LiveData<ProductModel?>
         get() = _productDetailsLiveData
 
-    fun getProductDetails(productId: String?) = _productDetailsLiveData.postValue(repository.getProductDetails(productId))
+    fun getProductDetails(productId: String?) =
+        _productDetailsLiveData.postValue(repository.getProductDetails(productId))
 
     private val _totalAmountLiveData = MutableLiveData<Double?>()
     val totalAmountLiveData: LiveData<Double?>
         get() = _totalAmountLiveData
-    fun getTotalAmount(productPrice: Double?, productQuantity: Int?) = _totalAmountLiveData.postValue(productPrice!! * productQuantity!!)
+
+    fun getTotalAmount(productPrice: Double?, productQuantity: Int?) =
+        _totalAmountLiveData.postValue(productPrice!! * productQuantity!!)
 }
