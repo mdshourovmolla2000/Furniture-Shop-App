f3a9a0560dfe87393e8cc301acc910b6de5ef800
mdshourovmolla2000 <68102562+mdshourovmolla2000@users.noreply.github.com>
2023-05-17 14:32:19 +0600

    signup function added

--- app/src/main/java/com/shourov/furnitureshop/view/ProfileFragment.kt
+++ app/src/main/java/com/shourov/furnitureshop/view/ProfileFragment.kt
@@ -1,12 +1,19 @@
 package com.shourov.furnitureshop.view
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.shourov.furnitureshop.R
+import com.shourov.furnitureshop.databinding.DialogLogoutBinding
 import com.shourov.furnitureshop.databinding.FragmentProfileBinding
+import com.shourov.furnitureshop.utils.SharedPref
+import com.shourov.furnitureshop.view.welcomeActivity.WelcomeActivity
 
 class ProfileFragment : Fragment() {
 
@@ -18,9 +25,39 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+        SharedPref.init(requireContext())
 
         binding.backIcon.setOnClickListener { findNavController().popBackStack() }
 
+        binding.logoutButton.setOnClickListener {
+            logoutDialog()
+        }
+
         return binding.root
+    }
+
+    private fun logoutDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        val dialogBinding = DialogLogoutBinding.inflate(layoutInflater)
+
+        builder.setView(dialogBinding.root)
+        builder.setCancelable(true)
+
+        val alertDialog = builder.create()
+
+        //make transparent to default dialog
+        alertDialog.window?.setBackgroundDrawable(ColorDrawable(0))
+
+        dialogBinding.noButton.setOnClickListener { alertDialog.dismiss() }
+
+        dialogBinding.yesButton.setOnClickListener {
+            alertDialog.dismiss()
+            SharedPref.write("IS_SIGNED_IN", "no")
+            startActivity(Intent(requireActivity(), WelcomeActivity::class.java))
+            requireActivity().overridePendingTransition(R.anim.enter, R.anim.exit)
+            requireActivity().finish()
+        }
+
+        alertDialog.show()
     }
 }
